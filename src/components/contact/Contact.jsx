3b8365b017f6130a004e0d79d84ea318b83f40ca
matthew <larochelle.matthew@gmail.com>
2023-03-29 12:35:04 -0700

    contact page layout

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,8 +4,18 @@ import styles from './Contact.module.css';
 function Contact() {
     return (
         <div>
-            Hello Contact!
-            <p>asdlfkjasdflhaweljkfhaslkdjf OMG</p>
+            <form className={styles.container}>
+                    <div className={styles.name}>
+                        <input type="text" placeholder="First Name" name="first_name"></input>
+                        <input type="text" placeholder="Last Name" name="last_name"></input>
+                    </div>
+                    <div className={styles.content}>
+                        <input type="email" placeholder="Email" name="email"></input>
+                        <input type="text" placeholder="Subject" name="subject"></input>
+                        <textarea rows="8" cols="80" placeholder="Message" className={styles.message} name="message"></textarea>
+                        <button className={styles.btn} type="submit">Submit</button>
+                    </div>
+                </form>
         </div>
     );
 }
