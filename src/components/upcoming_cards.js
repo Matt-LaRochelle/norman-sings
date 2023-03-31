540baf79b6df05aff9340475526ba7c58556e30e
matthew <larochelle.matthew@gmail.com>
2023-03-31 11:41:54 -0700

    add content to about page

--- src/components/upcoming_cards.js
+++ src/components/upcoming_cards.js
@@ -10,16 +10,6 @@ const cards = [
         performances: ["June 9th 7:30pm", "June 11th 2:00pm"],
         location: "Las Vegas, Nevada",
         image: olv
-    },
-
-    {
-        opera: "Tosca",
-        role: "Il Segrastano (Role Debut)",
-        link: "https://app.arts-people.com/index.php?clear_session=This+show+is+not+currently+available+for+purchase.+For+assistance+please+contact+our+box+office+at+714+731-8079.&theatre_id=3020",
-        company: "Sout Coast Symphony",
-        performances: ["March 25th 7:30pm"],
-        location: "Aliso Viejo, California",
-        image: scs
     }
 ]
 
