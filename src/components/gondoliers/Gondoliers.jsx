bc74c9c3c68798bb99b9fee7516cbc6575bbe608
matthew <larochelle.matthew@gmail.com>
2024-11-22 15:04:31 -0800

    unify button CSS and general tidying/refactoring CSS

--- src/components/gondoliers/Gondoliers.jsx
+++ src/components/gondoliers/Gondoliers.jsx
@@ -16,7 +16,6 @@ function About() {
                 <div className={styles.content}>
                     <p className={styles.p}>Besides being an international opera singer, Norman is also a gondolier at The Venetian Resort in Las Vegas Nevada. He works 5 days a week sharing his voice with his guests and giving them a beautiful experience. He works both on the inside and the outside ride and would love for you to come join him on his gondola.</p>
                     <p className={styles.p}>If you were already one of his guests, feel free to follow him on any of his social media platforms. He would also appreciate any gratuity you would like to provide him for the wonderful experience.. (Link below)</p>
-                    {/* Donate button */}
                     <button className={styles.btn} onClick={handleClick}>Donate</button>
                     <p className={styles.p}>Thank you for taking a ride and looking forward to your return!</p>
                 </div>
@@ -25,8 +24,6 @@ function About() {
                     <p className={styles.img_label}>Gondolier Rides, Las Vegas, NV</p>
                 </div>
             </div>
-            
-
         </div>
     );
 }
